70b80a7096dadcb988a02afbb31e54d39057ace9
Sachin / <sreesachin11226@gmail.com>
2025-05-13 09:38:49 +0530

    Add logger utility and environment constants; update package configuration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from "cors";
+import { config } from "dotenv"
+import { DEV_FRONTEND_URI, PROD_FRONTEND_URI } from './lib/constants';
+import logger from './lib/logger';
+config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  credentials: true,
+  origin: [
+    DEV_FRONTEND_URI,
+    PROD_FRONTEND_URI
+  ]
+}))
 
 app.get('/api/hello', (req, res) => {
   res.cookie('myCookie', 'delicious', {
@@ -15,4 +27,4 @@ app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from Express with TypeScript!' });
 });
 
-app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
+app.listen(PORT, () => logger.info('IMS Server running'));
---
+++ src/lib/constants.ts
@@ -0,0 +1,2 @@
+export const DEV_FRONTEND_URI = "http://localhost:8956"
+export const PROD_FRONTEND_URI = "https://ims.netlify.app"
